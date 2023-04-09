2d4a4724b1fdcdd9d63d33272b16e19031b6d3b5
srpss <61586478+srpss@users.noreply.github.com>
2023-04-09 19:53:20 +0300

    quill setup

---
+++ client/src/TextEditor.js
@@ -0,0 +1,22 @@
+import { useCallback} from 'react'
+import Quill from 'quill'
+import "quill/dist/quill.snow.css"
+
+
+export default function TextEditor() {
+   
+   
+    const wrapperRef = useCallback(wrapper=>{
+        if(wrapper == null) return
+        wrapper.innerHTML =''
+        const editor = document.createElement('div')
+        wrapper.append(editor)
+        new Quill(editor, {theme: "snow"})
+
+
+    },[])
+  return <div id="container" ref={wrapperRef}></div>
+  
+}
+
+
